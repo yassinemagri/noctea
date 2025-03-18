5a2f26de0e590cc8c44de38a9a9da8cf5b4f69d3
Yassine Magri <77500416+yassinemagri@users.noreply.github.com>
2025-03-18 00:14:17 +0000

    msg

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import { Button } from './components/ui/button'
 function App() {
 
   return (
-    <>
-     <Button>Hey</Button>
-    </>
+    <div className='flex'>
+    <h1>2004 - By: Say</h1>
+    <p>drari ana galss f galxy kanshm ri7at lkhrya lkhreha mol dar f sat7</p>
+     <Button>Hello World! salam wald l3lama</Button>
+    </div>
   )
 }
 
