5deb0088767158c57ade8bd038c23d734c51800d
Yassine Magri <77500416+yassinemagri@users.noreply.github.com>
2025-03-18 01:48:57 +0000

    example lkhdma

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
-import { useState } from 'react'
-import { Button } from './components/ui/button'
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+import { Route } from "react-router-dom";
 
 function App() {
-
+  const pages = [
+    {
+      id: "1",
+      page_name: "Mods",
+      page_url: "mods",
+    },
+    {
+      id: "2",
+      page_name: "Worlds",
+      page_url: "worlds",
+    },
+    {
+      id: "3",
+      page_name: "ResourcePacks",
+      page_url: "resource-packs",
+    },
+    {
+      id: "4",
+      page_name: "Shaders",
+      page_url: "shaders",
+    },
+  ];
+  
   return (
-    <div className='flex'>
-    <h1>2004 - By: Say</h1>
-    <p>drari ana galss f galxy kanshm ri7at lkhrya lkhreha mol dar f sat7</p>
-     <Button>Hello World! salam wald l3lama</Button>
+    <div className="flex">
+
+      {/* 
+            - galt nkhalik lik tstaf dyal files lik
+      Minecraft Pc:         mcecraft.com/java-edition
+      Minecraft telePhone:  mcecraft.com/bedrock
+      example ila khatrina mode d'pc mcecraft.com/java-edition/mods/mod-name 
+      */}
+      {/* 
+        <Route path="/shaders" element={<JavaEditionShaders />} /> 
+        
+      */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
