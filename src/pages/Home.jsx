9c61e474f6fc46833857768cb464768030da65b2
Lizeweski <128433107+LIZEWESKI@users.noreply.github.com>
2025-03-22 00:23:27 +0000

    testing api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import { JavaEdition } from './JavaEdition'
-
+import { mineCraftEndPoint } from '../api'
+import { useLoaderData } from 'react-router-dom';
+export async function loader() {
+  const data = await mineCraftEndPoint();
+  return data;
+} 
 const Home = () => {
+  const mineCraftEndPoint = useLoaderData();
+  console.log(mineCraftEndPoint)
   return (
     <div>
       <JavaEdition />
