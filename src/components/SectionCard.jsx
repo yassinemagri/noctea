cd493449a568a2468c22149e53afa390afbe36e4
Yassine Magri <77500416+yassinemagri@users.noreply.github.com>
2025-04-02 17:34:00 +0000

    debugging

--- src/components/SectionCard.jsx
+++ src/components/SectionCard.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom"
-import ModsCard from "./modsCard"
+import ModsCard from "./ModsCard"
 
 export default function PopularAddons({type, modCategory, filteredData}) {
 
